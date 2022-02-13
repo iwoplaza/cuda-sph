fba8712a5bd2c100d93debbc28d9b01078121ec3
czyjtu <maciejczyjtu@gmail.com>
2022-02-13 19:21:21 +0100

    Made dataclass frozen and fixed initialization with mutable default arguments

--- common/data_classes.py
+++ common/data_classes.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
 import numpy as np
-from typing import Tuple, List
+from typing import Tuple, List, Optional
 import config
 import logging 
 
 logger = logging.getLogger(__name__)
 
 
-@dataclass
+@dataclass(frozen=True)
 class Segment:
     start_point: Tuple[np.float64, np.float64, np.float64] = (0.0, 0.0, 0.0)
     start_radius: np.float64 = 1.0
@@ -21,7 +21,7 @@ class Segment:
         return np.array(segment_values, dtype=np.float64)
 
 
-@dataclass
+@dataclass(frozen=True)
 class Pipe:
     segments: List[Segment]
 
@@ -36,24 +36,28 @@ class Pipe:
         return np.stack(to_stack)
 
 
-@dataclass
+@dataclass(frozen=True)
 class SimulationParameters:
     n_particles: np.int32 = config.DEFAULT_N_PARTICLES
-    external_force: np.ndarray = config.DEFAULT_EXT_FORCE
+    external_force: np.ndarray = field(default_factory=lambda: config.DEFAULT_EXT_FORCE)
     simulation_duration: np.int32 = config.DEFAULT_DURATION  # in seconds
     fps: np.int32 = config.DEFAULT_FPS
     pipe: Pipe = field(default_factory=lambda: Pipe(segments=[Segment()]), repr=False)
-    space_size: np.ndarray = config.DEFAULT_SPACE_SIZE
-    voxel_size: np.ndarray = config.DEFAULT_VOXEL_SIZE
+    space_size: np.ndarray = field(default_factory=lambda: config.DEFAULT_SPACE_SIZE)
+    voxel_size: np.ndarray = field(default_factory=lambda: config.DEFAULT_VOXEL_SIZE)
 
     def __post_init__(self):
+        # make numpy arrays read-only 
+        self.external_force.setflags(write=False)
+        self.space_size.setflags(write=False)
+        self.voxel_size.setflags(write=False)
         logger.info(f"SimulationParameters object has been initialized to: {self}")
 
 
-@dataclass
+@dataclass(frozen=True)
 class SimulationState:
-    position: np.ndarray = None  # (n x 3)
-    velocity: np.ndarray = None  # (n x 3)
-    density:  np.ndarray = None  # (n)
+    position: Optional[np.ndarray] = None  # (n x 3)
+    velocity: Optional[np.ndarray] = None  # (n x 3)
+    density:  Optional[np.ndarray] = None  # (n)
 
 
