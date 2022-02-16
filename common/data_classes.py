62021d16d16c41ec7742ae3ea112507d8796da2d
czyjtu <maciejczyjtu@gmail.com>
2022-02-16 18:34:02 +0100

    Unfreeze dataclasses for now

--- common/data_classes.py
+++ common/data_classes.py
@@ -8,7 +8,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-@dataclass(frozen=True)
+@dataclass
 class Segment:
     start_point: Tuple[np.float64, np.float64, np.float64] = (0.0, 0.0, 0.0)
     start_radius: np.float64 = 1.0
@@ -21,7 +21,7 @@ class Segment:
         return np.array(segment_values, dtype=np.float64)
 
 
-@dataclass(frozen=True)
+@dataclass
 class Pipe:
     segments: List[Segment]
 
@@ -36,15 +36,15 @@ class Pipe:
         return np.stack(to_stack)
 
 
-@dataclass(frozen=True)
+@dataclass
 class SimulationParameters:
     n_particles: np.int32 = config.DEFAULT_N_PARTICLES
-    external_force: np.ndarray = field(default_factory=lambda: config.DEFAULT_EXT_FORCE)
+    external_force: np.ndarray = field(default_factory=lambda: np.copy(config.DEFAULT_EXT_FORCE))
     simulation_duration: np.int32 = config.DEFAULT_DURATION  # in seconds
     fps: np.int32 = config.DEFAULT_FPS
     pipe: Pipe = field(default_factory=lambda: Pipe(segments=[Segment()]), repr=False)
-    space_size: np.ndarray = field(default_factory=lambda: config.DEFAULT_SPACE_SIZE)
-    voxel_size: np.ndarray = field(default_factory=lambda: config.DEFAULT_VOXEL_SIZE)
+    space_size: np.ndarray = field(default_factory=lambda: np.copy(config.DEFAULT_SPACE_SIZE))
+    voxel_size: np.ndarray = field(default_factory=lambda: np.copy(config.DEFAULT_VOXEL_SIZE))
 
     def __post_init__(self):
         # make numpy arrays read-only 
@@ -54,7 +54,7 @@ class SimulationParameters:
         logger.info(f"SimulationParameters object has been initialized to: {self}")
 
 
-@dataclass(frozen=True)
+@dataclass
 class SimulationState:
     position: Optional[np.ndarray] = None  # (n x 3)
     velocity: Optional[np.ndarray] = None  # (n x 3)
