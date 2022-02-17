da0989503d83f124be23f9a499e1d4cf6dec7660
Iwo Plaza <iwoplaza@gmail.com>
2022-02-17 13:35:12 +0100

    Reimplemented the logger.

---
+++ sim/src/sph/logging_utils.py
@@ -0,0 +1,14 @@
+from timeit import default_timer as timer
+
+
+def sph_stage_logger(logger):
+    def decorator(method):
+        def wrapper(*args, **kwargs):
+            logger.info(f"calling {method.__name__} function")
+            s = timer()
+            result = method(*args, **kwargs)
+            logger.info(f"finished {method.__name__} call in {timer() - s:.4f} seconds")
+
+            return result
+        return wrapper
+    return decorator 
